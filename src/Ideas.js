14e703fe1a9591790b046a8d09bc40cf533d840a
Terra Manning <terradmanning@gmail.com>
2024-11-18 13:24:49 -0700

    feat: basic card, ideas and app components set up

---
+++ src/Ideas.js
@@ -0,0 +1,16 @@
+import Card from './Card';
+import './Ideas.css';
+
+function Ideas(props){
+  const { name } = props;
+
+  return (
+    <div className='ideas-container'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default Ideas;
